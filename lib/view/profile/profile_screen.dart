48e6afdef46fc2a358a8d0154a88131bcd3ff247
theakhilsarkar18 <akhilsodvadiya7@gmail.com>
2024-03-24 12:01:30 +0530

    24-03-2024

--- lib/view/profile/profile_screen.dart
+++ lib/view/profile/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:skyer/utils/colors.dart';
 import 'package:skyer/utils/globals.dart';
 
@@ -17,6 +18,10 @@ class ProfileScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
+        title: Text(
+          'Profile',
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
         actions: [
           IconButton(
             onPressed: () {},
@@ -27,15 +32,104 @@ class ProfileScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
+      body: ListView(
         children: [
           profilePicAndDetails(height, width, context),
           profileStatsRow(height, width, context),
           const Divider(
             thickness: 0.3,
           ),
+          Padding(
+            padding: EdgeInsets.all(Globals.defaultPadding),
+            child: StaggeredGrid.count(
+              crossAxisCount: 3,
+              crossAxisSpacing: Globals.defaultPadding,
+              mainAxisSpacing: Globals.defaultPadding,
+              children: [
+                gridImageBox(
+                  crossCount: 2,
+                  mainCount: 2,
+                  imageUrl: Globals.postImageUrl1,
+                ),
+                gridImageBox(
+                  crossCount: 1,
+                  mainCount: 1,
+                  imageUrl: Globals.postImageUrl2,
+                ),
+                gridImageBox(
+                  crossCount: 1,
+                  mainCount: 1,
+                  imageUrl: Globals.postImageUrl3,
+                ),
+                gridImageBox(
+                  crossCount: 1,
+                  mainCount: 1,
+                  imageUrl: Globals.postImageUrl3,
+                ),
+                gridImageBox(
+                  crossCount: 1,
+                  mainCount: 1,
+                  imageUrl: Globals.postImageUrl3,
+                ),
+                gridImageBox(
+                  crossCount: 1,
+                  mainCount: 1,
+                  imageUrl: Globals.postImageUrl3,
+                ),
+                gridImageBox(
+                  crossCount: 2,
+                  mainCount: 2,
+                  imageUrl: Globals.postImageUrl1,
+                ),
+                gridImageBox(
+                  crossCount: 1,
+                  mainCount: 1,
+                  imageUrl: Globals.postImageUrl2,
+                ),
+                gridImageBox(
+                  crossCount: 1,
+                  mainCount: 1,
+                  imageUrl: Globals.postImageUrl3,
+                ),
+                gridImageBox(
+                  crossCount: 1,
+                  mainCount: 1,
+                  imageUrl: Globals.postImageUrl3,
+                ),
+                gridImageBox(
+                  crossCount: 1,
+                  mainCount: 1,
+                  imageUrl: Globals.postImageUrl3,
+                ),
+                gridImageBox(
+                  crossCount: 1,
+                  mainCount: 1,
+                  imageUrl: Globals.postImageUrl3,
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
   }
+}
+
+Widget gridImageBox(
+    {required int crossCount,
+    required int mainCount,
+    required String imageUrl}) {
+  return StaggeredGridTile.count(
+    mainAxisCellCount: mainCount,
+    crossAxisCellCount: crossCount,
+    child: Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(Globals.defaultPadding),
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: NetworkImage(imageUrl),
+        ),
+      ),
+    ),
+  );
 }
