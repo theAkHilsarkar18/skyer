a6cf97c036c6dea64ffafd84d006d7d092605ee2
theAkHilsarkar18 <akhilsodvadiya7@gmail.com>
2024-04-10 19:42:46 +0530

    10/04/2024

---
+++ lib/firebase_services/firebase_model.dart
@@ -0,0 +1,19 @@
+class FirebaseModel
+{
+  Map? user;
+  List? followers;
+  List? following;
+  List? posts;
+}
+
+class UserModel
+{
+  String? name;
+  String? username;
+  String? email;
+  String? password;
+  String? city;
+  String? country;
+
+}
+
