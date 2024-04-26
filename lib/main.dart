8261c2911d7c497ab2fc32acc116043e3d53bf9e
theAkHilsarkar18 <akhilsodvadiya7@gmail.com>
2024-04-26 10:27:30 +0530

    19/04/2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:skyer/theme/animation_style.dart';
 import 'package:skyer/theme/theme_data_file.dart';
-import 'package:skyer/utils/colors.dart';
 import 'package:skyer/utils/routes_file.dart';
 import 'package:skyer/view/add%20post/provider/add_post_provider.dart';
-import 'package:skyer/view/home/home_screen.dart';
 import 'package:skyer/view/navigation%20bar/nav_bar_provider.dart';
-import 'package:skyer/view/navigation%20bar/nav_bar_screen.dart';
 import 'package:skyer/view/notification/provider/notification_provider.dart';
-import 'package:skyer/view/post%20view/post_view.dart';
-import 'package:skyer/view/profile/profile_screen.dart';
-import 'package:skyer/view/search/search_screen.dart';
 
 import 'firebase_options.dart';
 
@@ -50,3 +43,4 @@ class SkyerApp extends StatelessWidget {
     );
   }
 }
+
